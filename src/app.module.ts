9517e3d94230e4695aa3098ea7485399f206c812
Hammad Ali <hammad1019ali786@gmail.com>
2024-01-19 00:47:02 +0500

    Add Db connection and update authentication and authurization logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/domains/users/users.module.ts
+++ src/domains/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
 
 @Module({
   providers: [UsersService],
   exports: [UsersService],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
 })
 export class UsersModule {}
